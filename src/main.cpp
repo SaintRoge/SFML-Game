990f234085d788dd6a66b236be0b3ad134c524ad
SaintRoge <lemondesansfil@gmail.com>
2016-12-30 21:47:57 +0100

    Window added

--- src/main.cpp
+++ src/main.cpp
@@ -3,5 +3,17 @@
 using namespace sf;
 
 int main(int argc, char const *argv[]) {
-
+  RenderWindow window(VideoMode(1200, 800), "Tank", Style::Close | Style::Resize);
+  while (window.isOpen()) {
+    Event event;
+    while (window.pollEvent(event)) {
+      if (event.type == Event::Closed) {
+	window.close();
+      }
+      if (event.type == Event::Resized) {
+	FloatRect visibleArea(0, 0, event.size.width, event.size.height);
+	window.setView(View(visibleArea));
+      }
+    }
+  }
 }
